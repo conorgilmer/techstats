fc14f4cf98b841de358e2dd42bf9634f3ee92f7a
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-26 19:29:07 +0100

    index

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- header -->
+<?php include('php/header.php'); ?>  
+
+<!-- additional header stuff -->
+
+<!-- menu -->
+<?php include('php/menu.php'); ?>  
+
+
+<!-- Content Start -->
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Technology Used</h1>
+        <p class="lead">Just looking at what technologies i use.</p>
+        <!--p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p-->
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Software</h2>
+          <p>Plotting the s/w I use, editors, browsers, operating systems</p>
+          <p><a class="btn btn-primary" href="polls.php" role="button">View details &raquo;</a></p>
+        </div>
+
+        <div class="col-lg-4">
+          <h2>Sites</h2>
+          <p>Websites/Online cloud services I use</p>
+          <p><a class="btn btn-primary" href="sites.php" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+<!-- Content End -->
+
+<!-- footer -->
+<?php include('php/footer.php'); ?>  
