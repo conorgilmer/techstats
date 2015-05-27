7536938510740c5e27b884aa0d1c83369686f9b6
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-27 08:35:52 +0100

    title

--- php/header.php
+++ php/header.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>Irish Elections and Polls</title>
+    <title>Tech Stats</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
