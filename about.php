e51b2f38bfe4ecd92834f56d6f8409786866fffd
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-27 16:45:20 +0100

    about

---
+++ about.php
@@ -0,0 +1,20 @@
+
+<!-- header  -->
+<?php include('php/header.php'); ?> 
+
+<!-- menu  -->
+<?php include('php/menu.php');  ?>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-9">
+          <h2>About</h2>
+<p>This is just a rough record of applications, operating systems, and programming languages I use.
+</p>
+<p>I really note if I used an technology each day and not time or percentage of time i use them.</p>
+      </div>
+      </div>
+
+
+<!-- footer -->
+<?php include('php/footer.php');  ?>
